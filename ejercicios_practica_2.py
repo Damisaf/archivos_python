ff9c790ded501db1bcea2dda553385a8789f91b9
Damisaf <109141323+Damisaf@users.noreply.github.com>
2022-08-25 15:34:46 -0300

    Add files via upload

--- ejercicios_practica_2.py
+++ ejercicios_practica_2.py
@@ -10,67 +10,64 @@
 # Ejercicios con archivos
 
 import csv
+from pickle import NONE
 
 
 def ej3():
     print('Ejercicio de archivos CSV 1º')
-    #archivo = 'stock.csv'
-    
+    # archivo = 'stock.csv'
     # Realice un programa que abra el archivo 'stock.csv'
     # en modo lectura y cuente el stock total de tornillos
-    # a lo largo de todo el archivo, 
+    # a lo largo de todo el archivo,
     # sumando el stock en cada fila del archivo
-
     # Para eso debe leer los datos del archivo
     # con "csv.DictReader", y luego recorrer los datos
     # dentro de un bucle y solo acceder a la columna "tornillos"
     # para cumplir con el enunciado del ejercicio
-
     # Comenzar aquí, recuerde el identado dentro de esta funcion
-    archivo = open('stock.csv','r')
-    stock = list(csv.DictReader(archivo))
-    archivo.close()
-    total_tornillos = 0
-    for st in stock:
-        total_tornillos += int(st['tornillos'])
-    print ('el total de tornillos es: ', total_tornillos)
+    try:
+        archivo = open('stock.csv', 'r')
+        stock = list(csv.DictReader(archivo))
+        archivo.close()
+        total_tornillos = 0
+        for st in stock:
+            total_tornillos += int(st['tornillos'])
+        print ('el total de tornillos es: ', total_tornillos)
+    except:
+        print('falta el archivo stock.csv')
 
 
 def ej4():
     print('Ejercicios con archivos CSV 2º')
-    #archivo = 'propiedades.csv'
-    
+    # archivo = 'propiedades.csv'
     # Realice un programa que abra el archivo CSV "propiedades.csv"
     # en modo lectura. Recorrar dicho archivo y contar
     # la cantidad de departamentos de 2 ambientes y la cantidad
     # de departamentos de 3 ambientes disponibles.
     # Al finalizar el proceso, imprima en pantalla los resultados.
-
     # Tener cuidado que hay departamentos que no tienen definidos
     # la cantidad de ambientes, verifique el texto no esté vacio
     # antes de convertirlo a entero con "int( .. )"
     # NOTA: Si desea investigar puede evitar que el programa explote
     # utilizando "try except", tema que se verá la clase que viene.
-
     # Comenzar aquí, recuerde el identado dentro de esta funcion
-
-    with open('propiedades.csv','r') as arch_prop:
+    try:
+        arch_prop = open('propiedades.csv', 'r')
         prop = list(csv.DictReader(arch_prop))
         arch_prop.close()
-    
-    cant_2amb = 0
-    cant_3amb = 0
-    for pr in prop:
-        ambientes = pr['ambientes']
-        if ambientes == '2':
-            cant_2amb += 1
-        if ambientes == '3':
-            cant_3amb += 1    
-    print('cantidad de 2 ambientes: ', cant_2amb)
-    print('cantidad de 3 ambientes: ', cant_3amb)
 
-
-
+        cant_2amb = 0
+        cant_3amb = 0
+        for pr in prop:
+            ambientes = pr['ambientes']
+            if ambientes == '2':
+                cant_2amb += 1
+            if ambientes == '3':
+                cant_3amb += 1
+        print('cantidad de 2 ambientes: ', cant_2amb)
+        print('cantidad de 3 ambientes: ', cant_3amb)
+    except:
+        print('falta el archivo propiedades.csv')
 
 
 if __name__ == '__main__':
